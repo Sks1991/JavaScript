db249a3729a21be4127a2cca958adda984966f27
Sks1991 <sudhirk58@gmail.com>
2026-07-21 22:21:38 +0530

    dataType-summary

---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,54 @@
+// # Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+// JS is Dynamically typed language.
+
+// const score = 100               // => 100
+// const scoreValue = 100.3        // => 100.3
+// const isLoggedIn = false        // => false
+// const outSideTemp = null        // => null
+// let userEmail;                  // => undefined
+
+// console.table([score, scoreValue, isLoggedIn, outSideTemp, userEmail]);
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol;
+// console.log(id === anotherId);
+
+// const bigNumber = 395269523658923539n
+// console.log(typeof bigNumber);           // => false
+
+
+
+
+// # Reference / Non-premitive
+
+// Array, Object, Functions, Date, RegExp, Map, Set.
+
+
+// Array
+const heros = ["shaktiman", "naagraj", "doga"]
+console.log(heros);
+
+// Object
+let myObj = {
+     name: "Sudhir",
+     age: 22,
+}
+console.log(myObj);
+
+// Function
+const myFunction = function() {
+    console.log("Sudhir");
+}
+
+
+
+
+
+
+
+
+
